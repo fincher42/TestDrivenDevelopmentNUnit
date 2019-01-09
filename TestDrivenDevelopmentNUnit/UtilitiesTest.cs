3d8f74b248ba310fd5f18c079c09a9c4bebd4131
Mitch Fincher <mitchell.fincher@gm.com>
2019-01-09 10:23:39 -0600

    first commit

---
+++ TestDrivenDevelopmentNUnit/UtilitiesTest.cs
@@ -0,0 +1,54 @@
+using System;
+using NUnit.Framework;
+using Zoo;
+
+namespace Test.Zoo
+{
+    /*
+       Main Story Line:
+       We just bought a zoo and employ jr. high students to write queries against our db for our web site
+       to create pages based on themes, like all animals from reptiles from Asia.
+       But our students have a hard time with the URL syntax and we see bad queries like 
+       https://WeBoughtAZoo.com/api&location=Asia
+       https://WeBoughtAZoo.com/api&location=South America?type=reptile&pen=a2#4&time
+       
+       We need a helper URL builder function to make errors less likely, something like,
+       string url = Utilities.BuildUrl(host, name1, value1, name2, value2, ....)
+        
+     Five Workshop tasks: 
+         Your job is to incrementally build and test a method called "BuildUrl"
+         that will iteratively pass the following tests.  
+         One person should write the test, another make it pass.  
+         With 5 tasks, 10 people can participate.
+         Each time you update the BuildUrl, change it just enough to pass the test.
+     1. Passing a single, simple name-value pair:
+        string url = BuildUrl(host, "location","Africa");
+     2. Name-value pair with non-url friendly characters:
+        string url = BuildUrl(host, "animal pen","#16"); //(note: # in hex is 23)
+     3. Multiple name-value pairs
+        string url = BuildUrl(host, "type","mammal","continent","Africa","size","small","food appetite","carnivore","animal pen","#16");
+       code error conditions:
+     4. Check for bad number of parameters
+        string url = BuildUrl(host, "type","mammal","continent");
+     5. Check for bad host value
+    */
+    [TestFixture]
+    public class UtilitiesTest
+    {
+        //example for task 1.
+        [Test]
+        public void BuildUrl_SimpleUrl_Good_Input()
+        {
+            // Arrange
+            string host = "https://www.WeBoughtAZoo.com/api";
+            string name = "location";
+            string value = "Africa";
+
+            // Act
+            var url = Utilities.BuildUrl(host, name, value);
+            Console.Out.WriteLine("url = {0}", url);
+            // Assert
+            Assert.AreEqual("https://www.WeBoughtAZoo.com/api?location=Africa", url);
+        }
+    }
+}
---
+++ Zoo/Utilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace Zoo
+{
+    public static class Utilities
+    {
+        public static string BuildUrl(string host, string name, string value)
+        {
+            return $"{host}?{HttpUtility.UrlEncode(name)}={HttpUtility.UrlEncode(value)}";
+        }
+    }
+}
